eb7407c076f4175d56398ec5326fb22c92f3bb19
zhanggenrui <461667405@qq.com>
2022-03-27 19:45:09 +0800

    串口

---
+++ planet/Can.h
@@ -0,0 +1,17 @@
+//#ifndef CAN_H
+//#define CAN_H
+//
+//
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <unistd.h>
+//#include <net/if.h>
+//#include <sys/ioctl.h>
+//#include <sys/socket.h>
+//#include <linux/can.h>
+//#include <linux/can/raw.h>
+//#include <iostream>
+//
+//int Can(int order);
+//#endif // CAN_H
